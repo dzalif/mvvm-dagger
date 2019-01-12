3c165d035ef8d1a373d1a29495e6c6db40ecbfb2
dzalif <aliffirdhauz@gmail.com>
2019-01-12 13:31:06 +0700

    add activity scope

---
+++ app/src/main/java/io/neverstoplearning/advancedandroid/base/ActivityBindingModule.java
@@ -0,0 +1,23 @@
+package io.neverstoplearning.advancedandroid.base;
+
+import android.app.Activity;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.android.ActivityKey;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.IntoMap;
+import io.neverstoplearning.advancedandroid.home.MainActivity;
+import io.neverstoplearning.advancedandroid.home.MainActivityComponent;
+
+@Module(subcomponents = {
+        MainActivityComponent.class,
+})
+public abstract class ActivityBindingModule {
+
+    @Binds
+    @IntoMap
+    @ActivityKey(MainActivity.class)
+    abstract AndroidInjector.Factory<? extends Activity> provideMainActivityInjector(MainActivityComponent.Builder builder);
+    
+}
---
+++ app/src/main/java/io/neverstoplearning/advancedandroid/home/MainActivityComponent.java
@@ -0,0 +1,15 @@
+package io.neverstoplearning.advancedandroid.home;
+
+import dagger.Subcomponent;
+import dagger.android.AndroidInjector;
+import io.neverstoplearning.advancedandroid.di.ActivityScope;
+
+@ActivityScope
+@Subcomponent
+public interface MainActivityComponent extends AndroidInjector<MainActivity> {
+
+    @Subcomponent.Builder
+    abstract class Builder extends AndroidInjector.Builder<MainActivity> {
+
+    }
+}
