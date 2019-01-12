cd29d93e143856bc6811109764bad237aed3e613
dzalif <aliffirdhauz@gmail.com>
2019-01-12 13:18:42 +0700

    create application scope

---
+++ app/src/main/java/io/neverstoplearning/advancedandroid/base/ApplicationModule.java
@@ -0,0 +1,23 @@
+package io.neverstoplearning.advancedandroid.base;
+
+import android.app.Application;
+import android.content.Context;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class ApplicationModule {
+    private final Application application;
+
+    public ApplicationModule(Application application) {
+        this.application = application;
+    }
+
+    @Provides
+    Context provideApplicationContext(){
+        return application;
+    }
+
+
+}
---
+++ app/src/main/java/io/neverstoplearning/advancedandroid/base/MyApplication.java
@@ -0,0 +1,17 @@
+package io.neverstoplearning.advancedandroid.base;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    private ApplicationComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        component = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
+
+}
